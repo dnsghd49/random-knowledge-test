dfdb3eb24ef16a9e156542a3242b70ec78109829
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-29 19:21:03 -0500

    will going to work on the server.js later when the game is somewhat playable

--- server.js
+++
@@ -1,7 +0,0 @@
-app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "index.html"));
-  });
-
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
