9ac0ec8300ab11993f60221a1d9d9b798f4a426b
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-01 17:48:07 -0500

    the app now displays the questions and answer choices

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,37 @@
-// const test = document.getElementById("Choice")
-const currentQuestionIndex = 0
-let title = document.querySelector("h1")
+//Dom Elements 
+const question = document.querySelector('#question')
+const choices = Array.from(document.querySelectorAll('.choice'))
+const MAX_QUESTIONS = 4
 
 
+let currentQuestion = {}
+let acceptingAnswers = true
+let availableQuestions = []
+
+//Getting questions values  
 function start() {
-    let questions = [
-        {
-            question: "wat",
-            choice: "1",
-            choice: "2",
-            choice: "3",
-            choice: "4"
-        }
-    ]
+    availableQuestions = [...questions]
+    displayQnA()
+}
 
-    let currentQuestion = questions[currentQuestionIndex]
-    title.innertext = currentQuestion.question
-    document.append()
-    // test.innertext = ""
+
+//Tracking the score
+function displayQnA() {
+
+//Asking on track question 
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+//Takes the choice that user picked
+    choices.forEach(choice => {
+        const number = choice.dataset ['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
 }
 
 start()
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,13 +1,42 @@
-let questions = [
+const questions = [
     {
-        question: "wat",
-        choice: "1",
-        choice: "2",
-        choice: "3",
-        choice: "4"
+        question: "Commonly used data types DO NOT include:",
+        choice1: 'strings',
+        choice2: 'booleans',
+        choice3: 'alerts',
+        choice4: 'numbers',
+        answer: 3,
     },
-    
     {
-
+        question: "The condition in an if / else statement is enclosed within ____.",
+        choice1: 'quotes',
+        choice2: 'curly brackets',
+        choice3: 'parentheses',
+        choice4: 'square brackets',
+        answer: 3,
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        choice1: "numbers and strings",
+        choice2: "other arrays",
+        choice3: "booleans",
+        choice4: "all of the above",
+        answer: 4,
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choice1: "commas",
+        choice2: "curly brackets",
+        choice3: "quotes",
+        choice4: "parentheses",
+        answer: 3,
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choice1: "JavaScript",
+        choice2: "terminal / bash",
+        choice3: "for loops",
+        choice4: "console.log",
+        answer: 4,
     }
 ]
