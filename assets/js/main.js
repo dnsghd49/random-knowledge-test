8ebd9f0a919a211f7b1fd59b108bb9767e7b7338
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-01 19:08:04 -0500

    when user clicks a choice, it will go to the next question, also added a question counter to keep on track which questions is the user on

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
 //Dom Elements 
 const question = document.querySelector('#question')
 const choices = Array.from(document.querySelectorAll('.choice'))
+const progressText = document.querySelector('#questionProg');
+
 const MAX_QUESTIONS = 4
 
 
 let currentQuestion = {}
 let acceptingAnswers = true
 let availableQuestions = []
+let questionCounter = 0
 
 //Getting questions values  
 function start() {
@@ -18,20 +21,30 @@ function start() {
 //Tracking the score
 function displayQnA() {
 
-//Asking on track question 
+    //Display question
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
 
-//Takes the choice that user picked
+    //Display choice
     choices.forEach(choice => {
-        const number = choice.dataset ['number']
+        const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
     })
 
+    //Display number of questions
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+
+    //A stopper to stop displaying the same questions
     availableQuestions.splice(questionsIndex, 1)
 
     acceptingAnswers = true
 }
+
+//Display next question on click of a choice
+choices.forEach(choice => {
+    choice.addEventListener('click', displayQnA)
+})
 
 start()
