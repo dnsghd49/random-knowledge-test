58567233821a759abc25dab901ddd27c804d41b7
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-04 11:13:51 -0500

    testing api post

--- assets/js/result.js
+++ assets/js/result.js
@@ -2,6 +2,8 @@ const mostRecentScore = localStorage.getItem('mostRecentScore')
 const finalScore = document.querySelector('#finalScore')
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const api_base_url = "https://highscores-api.azurewebsites.net/swagger/v1/swagger.json"
+const apiKey = "fc04308b-bc51-4df3-a1e0-6f8fa0076d03"
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
@@ -12,6 +14,27 @@ finalScore.innerText = mostRecentScore
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
 })
+
+async function sendHighScore(scoreData, UserId) {
+    let response = await fetch(api_base_url, {
+        method: "POST",
+        headers: {
+            "x-api-key": apiKey,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            Data: scoreData,
+            UserId: UserId
+        })
+    })
+    let data = await response.json()
+    //does new highscore show up?
+    console.log('new', data)
+
+    let highscores = await getHighScores();
+    console.log(highscores)
+
+}
 
 //Saving scores that user entered
 function saveHighScore(e) {
