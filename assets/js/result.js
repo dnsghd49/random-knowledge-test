1477922f41fff04d7eacc58aacb7ff16b3158ac4
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-03 22:47:05 -0500

    the result page now gets local storage data and displays on the page but it highscore page still needs some work to display scores there

---
+++ assets/js/result.js
@@ -0,0 +1,38 @@
+const mostRecentScore = localStorage.getItem('mostRecentScore')
+const finalScore = document.querySelector('#finalScore')
+const username = document.querySelector('#username')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+const MAX_HIGH_SCORES = 5
+
+finalScore.innerText = mostRecentScore
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
+})
+
+//Saving scores that user entered
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a, b) => {
+        return b.score - a.score
+    })
+
+    //Max user 
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('')
+
+
+}
