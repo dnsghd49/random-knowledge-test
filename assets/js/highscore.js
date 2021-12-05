177d38e248d2cbd9be4753ebd9b7ebc34d52b059
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-05 18:15:56 -0500

    bringing back the working version

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,29 +1,7 @@
 const highScoresList = document.querySelector('#highScoresList')
 const highScores = JSON.parse(localStorage.getItem("highScores")) || []
-const api_base_url = "https://highscores-api.azurewebsites.net/highscores"
-const apiKey = "fc04308b-bc51-4df3-a1e0-6f8fa0076d03"
 
-async function getHighScores() {
-    let response = await fetch(api_base_url, {
-        headers : {
-            "x-api-key": apiKey
-        }
-    })
-    let highscores = await response.json()
-
-    let test1 = highscores.id
-    let lists = document.createElement("li")
-    lists.innerHTML = test1
-    lists.appendChild(highScoresList)
-
-
-    console.log(highscores)
-}
-
-// let highscores = await getHighScores();
-// console.log(highscores)
-getHighScores()
-// highScoresList.innerHTML =
-//     highScores.map(score => {
-//         return `<li class="high-score">${score.name} - ${score.score}</li>`
-//     }).join("")
+highScoresList.innerHTML =
+    highScores.map(score => {
+        return `<li class="high-score">${score.name} - ${score.score}</li>`
+    }).join("")
--- assets/js/result.js
+++ assets/js/result.js
@@ -2,8 +2,6 @@ const mostRecentScore = localStorage.getItem('mostRecentScore')
 const finalScore = document.querySelector('#finalScore')
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
-const api_base_url = "https://highscores-api.azurewebsites.net/highscores"
-const apiKey = "fc04308b-bc51-4df3-a1e0-6f8fa0076d03"
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
@@ -11,52 +9,30 @@ const MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
 
-saveScoreBtn.addEventListener("click", sendHighScore())
-
-username.addEventListener("keyup", () => {
+username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
 })
 
-async function sendHighScore() {
-    let user = username.value
-    let response = await fetch(api_base_url, {
-        method: "POST",
-        headers: {
-            "x-api-key": apiKey,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            Data: user,
-            userId: mostRecentScore
-        })
-    })
-    let data = await response.json()
-    // return data
-    // console.log("new", data)
-}
-// sendHighScore(mostRecentScore, username)
-
-
 //Saving scores that user entered
-// function saveHighScore(e) {
-//     e.preventDefault()
+function saveHighScore(e) {
+    e.preventDefault()
 
-//     const score = {
-//         score: mostRecentScore,
-//         name: username.value
-//     }
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
 
-//     highScores.push(score)
+    highScores.push(score)
 
-//     highScores.sort((a, b) => {
-//         return b.score - a.score
-//     })
+    highScores.sort((a, b) => {
+        return b.score - a.score
+    })
 
-//     //Max user 
+    //Max user 
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('')
 
 
-//     localStorage.setItem("highScores", JSON.stringify(highScores))
-//     window.location.assign("")
-
-
-// }
+}
